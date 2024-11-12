77f37439b620481ffa5ac3f510fbbd92c1f1b1d5
Barbara Czyz <bczyz@box.com>
2024-11-12 20:22:15 +0100

    branch change

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ContentBytes', // Usually your GitHub org/user name.
   projectName: 'contentbytes.github.io', // Usually your repo name.
-  deploymentBranch: 'deployment-tests',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
