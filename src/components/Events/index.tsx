9c9a835d9811cab190f0cbcb6939b3d2ff9d02ca
Barbara Czyz <bczyz@box.com>
2024-11-13 12:12:46 +0100

    Update event list and test deployment

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -10,9 +10,14 @@ export default function Events() {
         <h1>Events</h1>
       </div>
       <div className={styles.event}>
+        <p className={styles["event-title"]}>Content Bytes KRK #03</p>
+        <p>26 November 2024</p>
+        <p>[👉 Upcoming]</p>
+      </div>
+      <div className={styles.event}>
         <p className={styles["event-title"]}>Content Bytes KRK x girls.js</p>
         <p>30 October 2024</p>
-        <p>[👉 Upcoming]</p>
+        <p>[✅ Completed]</p>
       </div>
       <div className={styles.event}>
         <p className={styles["event-title"]}>Content Bytes KRK #01</p>
