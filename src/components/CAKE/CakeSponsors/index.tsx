32cff076668ef7537874c0538a84ebd65c766a11
szaganek <szaganek@gmail.com>
2025-04-05 21:48:05 +0200

    Fix imgs

--- src/components/CAKE/CakeSponsors/index.tsx
+++ src/components/CAKE/CakeSponsors/index.tsx
@@ -1,33 +1,31 @@
 import Link from "@docusaurus/Link";
 import styles from "./styles.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
 
 export default function CakePartners() {
   return (
     <div className={styles.background}>
       <div className={styles.title}>Media partners</div>
       <div className={styles.centerImgs}>
-        <div>
-          <Link
-            to="https://techwriterkoduje.pl"
-            title="Go to techwriterkoduje.pl"
-          >
-            <img
-              src="img/partners/techwriter-koduje.png"
-              alt="Go to techwriterkoduje.pl"
-              className={styles.image}
-            />
-          </Link>
-        </div>
+        <Link
+          to="https://techwriterkoduje.pl"
+          title="Go to techwriterkoduje.pl"
+        >
+          <img
+            src={useBaseUrl("img/partners/techwriter-koduje.png")}
+            alt="Go to techwriterkoduje.pl"
+            className={styles.image}
+          />
+        </Link>
         <div className={styles.divider} />
-        <div>
-          <Link to="https://techwriter.pl" title="Go to techwriter.pl">
-            <img
-              src="img/partners/techwriter-pl.png"
-              alt="Go to techwriter.pl"
-              className={styles.image}
-            />
-          </Link>
-        </div>
+        <Link to="https://techwriter.pl" title="Go to techwriter.pl">
+          <img
+            src={useBaseUrl("img/partners/techwriter-pl.png")}
+            alt="Go to techwriter.pl"
+            className={styles.image}
+          />
+        </Link>
       </div>
     </div>
   );
