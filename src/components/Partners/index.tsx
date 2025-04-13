22c8d201f67aee495e1520396a9d700056587bd9
szaganek <szaganek@gmail.com>
2025-04-13 12:20:23 +0200

    Add Forte Digital as a partner

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -1,52 +1,46 @@
-import clsx from "clsx";
-import styles from "./styles.module.css";
 import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
-type PartnerItem = {
-  label: string;
-  link: string;
-  imageSrc: string;
-};
+import styles from "./styles.module.css";
 
-const partnerList: PartnerItem[] = [
-  {
-    label: "Go to techwriterkoduje.pl",
-    link: "https://techwriterkoduje.pl",
-    imageSrc: "img/partners/techwriter-koduje.png",
-  },
-  {
-    label: "Go to techwriter.pl",
-    link: "https://techwriter.pl",
-    imageSrc: "img/partners/techwriter-pl.png",
-  },
-  {
-    label: "Go to apify.com",
-    link: "https://apify.com",
-    imageSrc: "img/partners/apify.png",
-  },
-];
-
-function Partner({ label, link, imageSrc }: PartnerItem) {
-  return (
-    <div className="col col--4">
-      <div className="col-demo">
-        <Link to={link} title={label}>
-          <img src={imageSrc} alt={label} className={styles.image} />
-        </Link>
-      </div>
-    </div>
-  );
-}
 
 export default function Partners() {
   return (
     <div id="partners" className={styles.background}>
       <div className="container">
         <h1 className={styles.title}>Partners</h1>
-        <div className={clsx("row", styles.partners)}>
-          {partnerList.map((props, idx) => (
-            <Partner key={idx} {...props} />
-          ))}
+        <div className={styles.partners}>
+          <Link
+            to="https://techwriterkoduje.pl"
+            title="Go to techwriterkoduje.pl"
+          >
+            <img
+              src={useBaseUrl("img/partners/techwriter-koduje.png")}
+              alt="Go to techwriterkoduje.pl"
+              className={styles.image}
+            />
+          </Link>
+          <Link to="https://techwriter.pl" title="Go to techwriter.pl">
+            <img
+              src={useBaseUrl("img/partners/techwriter-pl.png")}
+              alt="Go to techwriter.pl"
+              className={styles.image}
+            />
+          </Link>
+          <Link to="https://apify.com" title="Go to apify.com">
+            <img
+              src={useBaseUrl("img/partners/apify.png")}
+              alt="Go to apify.com"
+              className={styles.image}
+            />
+          </Link>
+          <Link to="https://fortedigital.com" title="Go to fortedigital.com">
+            <img
+              src={useBaseUrl("img/partners/forte.svg")}
+              alt="Go to fortedigital.com"
+              className={styles.image}
+            />
+          </Link>
         </div>
       </div>
     </div>
