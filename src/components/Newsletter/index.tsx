deb8e596e0bb67c80e7c207f0c9f71d10696ae58
szaganek <szaganek@gmail.com>
2025-04-15 22:03:20 +0200

    Add styling to newsletter embed

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,10 +1,24 @@
-import styles from './styles.module.css';
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 export default function Newsletter() {
   return (
-    <div className='container'>
-      <div><h1>.</h1></div>
-      <div className="ml-embedded" data-form="3GCsHv"></div>
+    <div className={clsx("container", styles.spaceBottom)}>
+      <div>
+        <h1 className={styles.title}>Get notified about upcoming events!</h1>
+      </div>
+      <div className={styles.center}>
+        <div className="ml-embedded" data-form="3GCsHv"></div>
+        <div className={styles.divider} />
+        <div>
+          <p>
+            Leave us your email and we'll keep you well-informed about what's
+            cooking 👨‍🍳
+          </p>
+          <p>No spam, only essential updates.</p>
+          <p>You can change your preferences any time.</p>
+        </div>
+      </div>
     </div>
   );
 }
