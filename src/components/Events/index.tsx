7dc230f1f6b03eeaa608b10ede41b4eb39f83834
szaganek <szaganek@gmail.com>
2025-06-03 20:51:12 +0200

    Add event no.9

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -3,27 +3,27 @@ import { ButtonBlack, ButtonWhite } from "../Button";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import clsx from "clsx";
 
-const EventPage = "events/2025/8";
-const SignupForm = "https://lu.ma/vnnxohf3";
+const EventPage = "events/2025/9";
+const SignupForm = "https://lu.ma/gjmlponl";
 
 export default function Events() {
   return (
     <div id="events" className={styles.background}>
       <div className="container">
         <div>
-          <h1 className="padding-bottom--lg">Last event</h1>
+          <h1 className="padding-bottom--lg">Next event</h1>
         </div>
         <div className="padding-bottom--lg">
           <img
             alt="Promotion image for the Content Bytes event."
-            src={useBaseUrl("img/events/event8.png")}
+            src={useBaseUrl("img/events/event9.png")}
             className={clsx(styles.image)}
           />
         </div>
         <div className={clsx(styles.buttons)}>
           <ButtonWhite link={EventPage} label="👀 Learn more" />
           <div className={styles.divider} />
-          {/* <ButtonBlack link={SignupForm} label="📨 Sign up" /> */}
+          <ButtonBlack link={SignupForm} label="📨 Sign up" />
         </div>
       </div>
     </div>
